0a63fa77ecf8fcada994caa977943a92f0129c22
Yu Feng <yfeng1@coma.hpc1.cs.cmu.edu>
2014-12-12 18:02:52 -0500

    change package name to gaepsi2

--- setup.py
+++ setup.py
@@ -5,19 +5,18 @@ import numpy
 def myext(*args):
     return Extension(*args, include_dirs=["./", numpy.get_include()])
 extensions = [
-        myext("gaepsi.svr", ["src/svr.pyx"]),
-        myext("gaepsi._domain", ["src/_domain.pyx"]),
-        myext("gaepsi.painter", ["src/painter.pyx"]),
-        myext("mpiimport.mpiimport", ["mpiimport/mpiimport.pyx"]),
+        myext("gaepsi2.svr", ["src/svr.pyx"]),
+        myext("gaepsi2._domain", ["src/_domain.pyx"]),
+        myext("gaepsi2.painter", ["src/painter.pyx"]),
         ]
 
 setup(
-    name="bigfilepy", version="0.1",
+    name="gaepsi2", version="0.1",
     author="Yu Feng",
-    description="python binding of BigFile, a peta scale IO format",
-    package_dir = {'gaepsi': 'src'},
+    description="gaepsi2",
+    package_dir = {'gaepsi2': 'src'},
     install_requires=['cython', 'numpy'],
-    packages= ['gaepsi'],
+    packages= ['gaepsi2'],
     requires=['numpy'],
     ext_modules = cythonize(extensions)
 )
